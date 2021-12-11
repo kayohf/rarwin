25bdeddaef1d21a1cab0398443832a987933dec0
Kayo Hamid <kayohamid@outlook.com>
2021-12-11 12:54:50 -0400

    init: begin of a dream

---
+++ rarwin/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace rarwin
+{
+    internal class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Input file to be processed.")]
+        public IEnumerable<string>? Input { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Output path.")]
+        public IEnumerable<string>? Output { get; set; }
+    }
+}
---
+++ rarwin/Program.cs
@@ -0,0 +1,32 @@
+﻿using rarwin;
+using CommandLine;
+using SharpCompress.Readers;
+using SharpCompress.Common;
+
+Parser.Default.ParseArguments<Options>(args)
+  .WithParsed(o =>
+  {
+      var path = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()?.Location);
+      var fileToExtract = o.Input?.First().ToString();
+
+      using var stream = File.OpenRead($"{path}\\{fileToExtract}");
+      var reader = ReaderFactory.Open(stream);
+
+	  var destPath = fileToExtract.Split('.');
+
+	  if (o.Output.Any()) destPath[0] = o.Output.First().ToString();
+
+	  if (!Directory.Exists($"{path}\\{destPath[0]}"))
+	  {
+		  Directory.CreateDirectory($"{path}\\{destPath[0]}");
+	  }
+
+	  while (reader.MoveToNextEntry())
+	  {
+		  if (!reader.Entry.IsDirectory)
+		  {
+			  Console.WriteLine(reader.Entry.Key);
+			  reader.WriteEntryToDirectory($"{destPath[0]}", new ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
+		  }
+	  }
+  });
